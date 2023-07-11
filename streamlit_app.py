ebe6918b2d1057307faddafd15223a936948d600
VirgileRakoto <139237082+VirgileRakoto@users.noreply.github.com>
2023-07-11 15:03:25 +0200

    Update streamlit_app.py
    add text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit 
 streamlit.title('My parent healthy dinner')
+streamlit.header('breakfast menu')
+streamlit.text('Omega 3 and bluberry oatmeal')
+streamlit.text('Kale, spinash & rocket smoothie')
+streamlit.text('Hard boiled free range Egg')
