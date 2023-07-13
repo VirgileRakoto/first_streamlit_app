a52f75924be9b1c3a5cb761b945c523aea095ffd
VirgileRakoto <139237082+VirgileRakoto@users.noreply.github.com>
2023-07-13 09:38:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas 
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
