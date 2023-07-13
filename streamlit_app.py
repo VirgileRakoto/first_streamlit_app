527b385b1689812a434e1c7f7d1f5607a02f2433
VirgileRakoto <139237082+VirgileRakoto@users.noreply.github.com>
2023-07-13 10:09:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
  
 
 def get_fruit_load_list():
- with my_cnx.cursor as my_cur:
+ with my_cnx.cursor() as my_cur:
   my_cur.execute("USE ROLE ACCOUNTADMIN ;")
   my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST ;")
   return my_cur.fetchall()
